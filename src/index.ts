fed200b4133f55c0c443ca68fbd755a06b9b3093
Tamas Nepusz <ntamas@gmail.com>
2019-11-12 14:21:28 +0100

    TypeGuard is now exported from src/types.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { looksLike } from "./looksLike";
 export { allOf, anyOf } from "./combiners";
 export { looksLike } from "./looksLike";
 export { arrayOf, instanceOf, maybeNil, maybeNull, optional } from "./modifiers";
+export { TypeGuard } from "./types";
 
 export default looksLike;
